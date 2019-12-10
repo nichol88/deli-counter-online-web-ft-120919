d8d7747730e1096dc1a2cebcc39a6a1e43ff42fc
Brian Nicholls <nichol88@gmail.com>
2019-12-10 00:31:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(l)
+  if l.empty?
+    puts "The line is currently empty."
+  else
+    x = []
+    l.each_with_index { |e, i|
+      x.push("#{i + 1}. #{e}")
+    }
+    puts "The line is currently: #{x.join(" ")}"
+  end
+end
+
+def take_a_number(l, name)
+  l.push(name)
+  puts "Welcome, #{name}. You are number #{l.length} in line."
+end
+
+def now_serving(l)
+  if l.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{l.shift}."
+  end
+end
